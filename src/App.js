909d422909c83f2770e6c97eac854defb495e800
liji356 <lijibthomas95@gmail.com>
2022-05-17 18:30:18 +0530

    searchbranch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Product from './component/Product';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Search from './component/Search';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
    <BrowserRouter>
    <Routes>
      <Route path='/' exact element={<Product/>}/>
+     <Route path='/search' exact element={<Search/>}/>
    </Routes>
    </BrowserRouter>
    </> 

5f79cd49d7f545dad2bdd2074e80ba87fa860aec
liji356 <lijibthomas95@gmail.com>
2022-05-17 19:16:05 +0530

    searchbranch

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Header from './Header'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div>
+
+        <Header/>
+
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Name</label>
+                    <input className='form-control'/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-info">SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
   )
 }
 
